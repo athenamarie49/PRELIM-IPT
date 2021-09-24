7050a944854c52f495d4e607cb44fa0c0c307e56
athenamarie49 <athenamariee49@gmail.com>
2021-09-24 21:24:55 +0800

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,67 @@
+@extends('base')
+
+@section('content')
+
+<style></style>
+
+  <div class="container"  style="width: 20rem;">
+  <h1 class="title text-dark"  >Korean Drama </h1>
+  </div>
+
+  <style>
+
+    .center {
+      padding: 70px 0;
+      border: 3px solid blue;
+      text-align: center;
+      margin-top: 15%;
+    }
+    </style>
+    <br>
+    <br>
+    <div class="container">
+    
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="card" style="width: 20rem;">
+              <img src="1.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Witch Dinner</h5>
+                <p class="card-text">Jung Jin is an ordinary 28-year-old woman with a job and a boyfriend, but her boyfriend dumps her and she gets fired from her job. Jung Jin then decides to open a restaurant with her mother. The restaurant goes bankrupt. Her mother goes down to the country and Jung Jin is left alone at the restaurant.
+                   At that time, Jo Hee-Ra (Song Ji-Hyo) appears in front of her. Jo Hee-Ra is a witch and she wants to borrow Jung Jin's restaurant to open her own restaurant. Jo Hee-Ra's restaurant would make food that grants a customer's wish. In exchange for granting their wish, the customers would give Jo Hee-Ra something that she wants from them. Jung Jin is skeptical at first, but she has an incredible experience from eating Jo Hee-Ra's food.
+                   </p>
+                <a href="#" class="btn btn-danger">Watch this</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 20rem;">
+              <img src="2.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Hello, Me!</h5>
+                <p class="card-text">Ban Ha-Ni (Choi Gang-Hee) is 37-years-old and a single woman. She holds a temporary position at work, which makes her nervous that she might get fired. She is generally servile and timid. Considering her current state, she does not want to be single.
+                   One day, 17-year-old Ban Ha-Ni (Lee Re) from the past comes to 37-year-old Ban Ha-Ni. 17-year-old Ban Ha-Ni is totally different from her older self. 
+                  The younger Ban Ha-Ni is bright and optimistic.</p>
+                <a href="#" class="btn btn-danger">Watch this</a>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card" style="width: 20rem;">
+              <img src="3.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-danger">Watch this</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+    
+    </div>
+@endsection
+
---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,18 @@
+
+@extends('base')
+
+@section('content')
+<style>
+  body{
+    background: url(background.gif);
+  background-repeat: no-repeat;
+  background-size: cover;
+  }
+</style>
+
+<div class="container" style="width: 50rem;">
+  </div>
+
+
+@endsection 
+  
